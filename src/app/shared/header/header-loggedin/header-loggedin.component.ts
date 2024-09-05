e98363aa9e91e0f32df491fd3f7bfc25a4ff9e92
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-05 19:57:37 -0300

    feat: create header logged in

---
+++ src/app/shared/header/header-loggedin/header-loggedin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-loggedin',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-loggedin.component.html',
+  styleUrl: './header-loggedin.component.scss'
+})
+export class HeaderLoggedinComponent {
+
+}
