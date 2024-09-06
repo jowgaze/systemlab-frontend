fe5029e997a82b107e4446ae56d4403699b3ef51
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-06 19:01:22 -0300

    feat: interaction with modal on buttons

--- src/app/shared/header/header-loggedout/header-loggedout.component.ts
+++ src/app/shared/header/header-loggedout/header-loggedout.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { ModalRegisterComponent } from '../../modal-register/modal-register.component';
+import { ModalLoginComponent } from '../../modal-login/modal-login.component';
 
 @Component({
   selector: 'app-header-loggedout',
   standalone: true,
-  imports: [],
+  imports: [ModalRegisterComponent, ModalLoginComponent],
   templateUrl: './header-loggedout.component.html',
   styleUrl: './header-loggedout.component.scss'
 })
-export class HeaderLoggedoutComponent {
+export class HeaderLoggedoutComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
 }
