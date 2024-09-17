6ff53bc1ad94c44225bdb1c950c7e72e8e4889fb
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-17 08:11:13 -0300

    style: class ordering

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { CardSessionComponent } from './../../shared/card-session/card-session.component';
 import { Component } from '@angular/core';
+
+import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderLoggedoutComponent } from '../../shared/header/header-loggedout/header-loggedout.component';
-import { FooterComponent } from "../../shared/footer/footer.component";
+import { CardSessionComponent } from './../../shared/card-session/card-session.component';
 
 @Component({
   selector: 'app-home',
