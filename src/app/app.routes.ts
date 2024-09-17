5c39382f86e3fef0a06ce35d152c7ae880033e71
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-17 08:13:24 -0300

    feat: add test routes + jwt decode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { SupervisorComponent } from './pages/supervisor/supervisor.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'supervisor', component: SupervisorComponent}
 ];
