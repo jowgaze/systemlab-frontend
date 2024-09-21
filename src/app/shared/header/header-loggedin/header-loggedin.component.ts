f5cd49d47d93513870c7df113fa52c2116817b09
jowgaze <jonatha.biomon.hp@gmail.com>
2024-09-20 21:33:57 -0300

    feat: implements logout

--- src/app/shared/header/header-loggedin/header-loggedin.component.ts
+++ src/app/shared/header/header-loggedin/header-loggedin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-loggedin',
@@ -9,4 +11,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderLoggedinComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
